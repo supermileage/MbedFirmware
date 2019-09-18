5aaed06b2227c61027f627d7964ad7e503756c65
Spencer Perkins <hello@spencerperkins.me>
2019-09-17 23:18:29 -0700

    Initialization

---
+++ README.md
@@ -0,0 +1,12 @@
+This is barebones can messages between to stm32l432's to trigger blinking lights
+
+Getting started:
+0. pip install mbed-cli 
+1. Clone the repo
+2. mbed deploy in repo
+3. pip install -r requirements.txt (recommned to use a virtualenv)
+4. mbed new .
+5. mbed target nucleo_l432kc
+6. mbed toolchain GCC_ARM
+7. mbed compile
+8. drag bin file into the microcontroller
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include "mbed.h"
+#include "mbed-os/targets/TARGET_STM/TARGET_STM32L4/TARGET_STM32L432xC/objects.h"
+
+DigitalOut myled(LED1);
+Serial pc(USBTX, USBRX);
+CAN can(D10, D2);
+
+void read();
+void send(const char* data);
+
+int main() {
+    while(true) {  
+        // send("some data");
+        read();
+    }
+}
+
+void read() {
+    CANMessage msg;
+    pc.printf("Checking \n");
+    if(can.read(msg)) {
+        pc.printf("Recieved, \n");
+        myled = !myled; 
+    }
+}
+
+void send(const char* data) {
+    pc.printf("Sending \n");
+    can.write(CANMessage(1,data,8));
+    myled = 1; 
+    wait(1); 
+    myled = 0;
+    wait(1); 
+}
